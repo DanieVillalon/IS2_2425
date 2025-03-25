e70e2b6db8f9839e9dfb783e152842a8a1e5f23a
egc719 <egc719@alumnos.unican.es>
2025-03-25 22:17:48 +0100

    Creada Clase VistaFuncionarioIT

---
+++ Practica_4A/ImpuestoCirculacionGUI/src/test/java/es/unican/is2/impuestoCirculacionGUI/VistaFuncionarioIT.java
@@ -0,0 +1,12 @@
+package es.unican.is2.impuestoCirculacionGUI;
+
+import org.fest.swing.fixture.FrameFixture;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class VistaFuncionarioIT {
+	
+
+
+}
