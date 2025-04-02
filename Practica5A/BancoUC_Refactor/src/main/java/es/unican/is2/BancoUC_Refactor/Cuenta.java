ef69edb31047bf4914f1be86b281c8a841d4c840
DanielVillalon <dvg136@alumnos.unican.es>
2025-04-02 12:35:03 +0200

    Comienzo de la práctica 5A

---
+++ Practica5A/BancoUC_Refactor/src/main/java/es/unican/is2/BancoUC_Refactor/Cuenta.java
@@ -0,0 +1,16 @@
+package es.unican.is2.BancoUC_Refactor;
+
+
+public class Cuenta {
+	
+	private String numCuenta;
+	
+	public Cuenta(String numCuenta) {
+		this.numCuenta = numCuenta;
+	}
+	
+	public String getNumCuenta() {
+		return numCuenta;
+	}
+	
+}
---
+++ Practica5A/BancoUC_Refactor/src/main/java/es/unican/is2/BancoUC_Refactor/datoErroneoException.java
@@ -0,0 +1,11 @@
+package es.unican.is2.BancoUC_Refactor;
+
+
+@SuppressWarnings("serial")
+public class datoErroneoException extends RuntimeException {
+	
+	public datoErroneoException (String mensaje) {
+		super(mensaje);
+	}
+
+}
